0aa5f0fa8a8d5b9d3c8faba2d9c08d74dbeb6313
CryptoCommander007 <rivantped@gmail.com>
2024-10-24 16:45:42 -0300

    PROYECTO-FOTO-EMPRENDE-BASE

---
+++ run_commands.bat
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "PROYECTO-FOTO-EMPRENDE-BASE"
+git branch -M main
+git remote add origin https://github.com/CryptoCommander007/PROYECTO-FOTO-EMPRENDE-BASE.git
+git push -u origin main
